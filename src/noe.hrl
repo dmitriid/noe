83f8f33bcdcdd7427c1cb1a6e8ebe5e2197b1ec8
Davide <nesrait@gmail.com>
2008-12-19 02:04:54 +0000

    Fixed references to the log function.

--- src/noe.hrl
+++ src/noe.hrl
@@ -1,10 +1,10 @@
 -define(L(Msg), io:format("~p:~b ~p ~n", [?MODULE, ?LINE, Msg])).
 
 -define(Debug(Msg, Params),
-	general_utils:log(?MODULE, ?LINE, debug, fun() -> {Msg, Params} end)).
+	noe:log(?MODULE, ?LINE, debug, fun() -> {Msg, Params} end)).
 -define(Info(Msg, Params),
- 	general_utils:log(?MODULE, ?LINE, info, fun() -> {Msg, Params} end)).
+ 	noe:log(?MODULE, ?LINE, info, fun() -> {Msg, Params} end)).
 -define(Warn(Msg, Params),
- 	general_utils:log(?MODULE, ?LINE, warn, fun() -> {Msg, Params} end)).
+ 	noe:log(?MODULE, ?LINE, warn, fun() -> {Msg, Params} end)).
 -define(Error(Msg, Params),
- 	general_utils:log(?MODULE, ?LINE, error, fun() -> {Msg, Params} end)).
+ 	noe:log(?MODULE, ?LINE, error, fun() -> {Msg, Params} end)).
